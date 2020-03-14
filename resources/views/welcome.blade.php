ddde50b4b01bb59b1a86fce86a40f23c85b47ba5
faiz-05 <0505faizazam@gmail.com>
2020-03-14 13:21:04 +0700

    merubah tampilan welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    workshop
+                    FAIZ
                 </div>
 
                 <div class="links">
